b95c2530589c867bc2488a8d500b66250660c9a9
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2022-05-16 02:25:27 -0400

    Get environment to support GPU

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 import os
 import PIL
@@ -12,6 +13,38 @@ print("Importing a bunch of stuff here")
 
 # get to files
 
+
 from pathlib import Path
-p = Path('.')
-print([x for x in p.iterdir() if x.is_dir()])
+data_dir = Path('data')
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+#hotdog = list(data_dir.glob('hotdog/*'))
+#image = mpimg.imread(str(hotdog[0]))
+#plt.imshow(image)
+#plt.show()
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+
+class_names = train_ds.class_names
+print(class_names)
+print(tf.version.VERSION)
